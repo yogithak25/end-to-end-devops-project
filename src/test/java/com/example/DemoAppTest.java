091a1e1e339dabeca744e658d14e2de73f481d38
yogithak25 <yogithak25@gmail.com>
2026-03-11 20:11:31 +0530

    Create DemoAppTest.java

---
+++ src/test/java/com/example/DemoAppTest.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class DemoAppTest {
+
+    @Test
+    public void testStartupMessage() {
+
+        assertEquals(
+                "End-to-End DevOps CI/CD Pipeline Deployment Successful",
+                DemoApp.getStartupMessage()
+        );
+    }
+}
