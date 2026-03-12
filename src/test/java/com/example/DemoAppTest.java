ee6adf6c9a8845bbf7e4682c08ac0ddc74428c60
yogithak25 <yogithak25@gmail.com>
2026-03-12 17:42:17 +0530

    Update DemoAppTest.java

--- src/test/java/com/example/DemoAppTest.java
+++ src/test/java/com/example/DemoAppTest.java
@@ -1,13 +1,6 @@
 package com.example;
 
-import com.sun.net.httpserver.HttpServer;
 import org.junit.Test;
-
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
 import static org.junit.Assert.*;
 
 public class DemoAppTest {
@@ -15,12 +8,11 @@ public class DemoAppTest {
     @Test
     public void testStartupMessage() {
 
-        String msg = DemoApp.getStartupMessage();
-
         assertEquals(
                 "End-to-End DevOps CI/CD Pipeline Deployment Successful",
-                msg
+                DemoApp.getStartupMessage()
         );
+
     }
 
     @Test
@@ -30,32 +22,7 @@ public class DemoAppTest {
 
         assertNotNull(html);
         assertTrue(html.length > 0);
+
     }
 
-    @Test
-    public void testHttpEndpoint() throws Exception {
-
-        int port = 9099;
-
-        HttpServer server = DemoApp.startServer(port);
-
-        URL url = new URL("http://localhost:" + port + "/");
-
-        HttpURLConnection conn =
-                (HttpURLConnection) url.openConnection();
-
-        conn.setRequestMethod("GET");
-
-        BufferedReader reader =
-                new BufferedReader(
-                        new InputStreamReader(conn.getInputStream()));
-
-        String line = reader.readLine();
-
-        reader.close();
-
-        assertNotNull(line);
-
-        server.stop(0);
-    }
 }
