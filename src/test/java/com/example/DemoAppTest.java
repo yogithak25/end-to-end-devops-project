8c35251ae61192e549de89eee53d76f139605dea
yogithak25 <yogithak25@gmail.com>
2026-03-12 16:47:41 +0530

    Update DemoAppTest.java

--- src/test/java/com/example/DemoAppTest.java
+++ src/test/java/com/example/DemoAppTest.java
@@ -7,17 +7,26 @@ public class DemoAppTest {
 
     @Test
     public void testStartupMessage() {
-
         assertEquals(
-                "End-to-End DevOps CI/CD Pipeline Deployment Successful",
-                DemoApp.getStartupMessage()
+            "End-to-End DevOps CI/CD Pipeline Deployment Successful",
+            DemoApp.getStartupMessage()
         );
     }
+
     @Test
-    public void testHtmlResponse() {
+    public void testHtmlResponseContainsMessage() {
 
         String html = DemoApp.getHtmlResponse();
 
         assertTrue(html.contains("DevOps"));
     }
+
+    @Test
+    public void testHtmlResponseNotNull() {
+
+        String html = DemoApp.getHtmlResponse();
+
+        assertNotNull(html);
+    }
+
 }
